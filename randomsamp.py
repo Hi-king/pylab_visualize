0a9cd8784300cb3f446696da6a1a8ddf26669940
Keisuke OGAKI <keisuke_ogaki@dwango.co.jp>
2013-10-10 19:53:48 +0900

    use O(n) algorithm

--- randomsamp.py
+++ randomsamp.py
@@ -3,14 +3,24 @@ import sys
 import math
 import random
 
-outputnum=int(sys.argv[1])
 lines = [line for line in sys.stdin]
+if(len(sys.argv)>1):
+    outputnum=int(sys.argv[1])
+else:
+    outputnum=len(lines)
 
 randomizedlinenum=[]
-while len(randomizedlinenum)<len(lines) and len(randomizedlinenum)<outputnum:
-    randomizedlinenum
-    randnum = random.randint(0, len(lines)-1)
-    if not randnum in randomizedlinenum:
-        randomizedlinenum.append(randnum)
-for linenum in randomizedlinenum:
-    print lines[linenum],
+for i in xrange(outputnum):
+    randi = random.randint(i, len(lines)-1)
+    lines[i], lines[randi] = lines[randi], lines[i]
+for i in xrange(outputnum):
+    print lines[i],
+
+
+# while len(randomizedlinenum)<len(lines) and len(randomizedlinenum)<outputnum:
+#     randomizedlinenum
+#     randnum = random.randint(0, len(lines)-1)
+#     if not randnum in randomizedlinenum:
+#         randomizedlinenum.append(randnum)
+# for linenum in randomizedlinenum:
+#     print lines[linenum],
